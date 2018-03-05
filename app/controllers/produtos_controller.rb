a45001ad425e47d1fba656ca920e74c1600174f7
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-03-04 21:56:12 -0300

    Cadastro de produtos básico

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,6 +4,9 @@ class ProdutosController < ApplicationController
         @listaProduto = Produto.order(:nome).limit 10
     end
 
-
+    def create
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade_estoque
+        produto = Produto.create valores
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #RAIZ
   root "produtos#index"
+  
+  #Pagina PRODUTO
+  get "/produtos/novo" => "produtos#cadastro"
+  post "/produtos" => "produtos#create"
+
 end
