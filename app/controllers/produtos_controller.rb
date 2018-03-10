0a9b4889181be95c52f0b22aa943f814714f7711
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-03-10 17:46:42 -0300

    Adiconando busca na navbar e utilizando resources para criação das rotas

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -7,13 +7,18 @@ class ProdutosController < ApplicationController
     def create
         valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade_estoque
         produto = Produto.create valores
-        redirect_to produtos_novo_url
+        redirect_to new_produto_url
     end
 
     def destroy
         id = params[:id]
         Produto.destroy id
         redirect_to root_url
+    end
+
+    def busca
+        @nome_busca = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome_busca}%"
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #Pagina PRODUTO
+  resources :produtos, only: [:new, :create, :destroy]
+  post "/produtos/busca" => "produtos#busca", as: :busca_produto
+  get "/produtos/busca" => "produtos#busca"
   #RAIZ
   root "produtos#index"
   
-  #Pagina PRODUTO
-  get "/produtos/new" => "produtos#cadastro"
-  delete "produtos/:id" => "produtos#destroy", as: :produto
-  post "/produtos" => "produtos#create"
 
 end
