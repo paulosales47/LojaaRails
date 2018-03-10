3f65aa5e958f9941735c6ab11cc7693788ebef6e
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-03-10 19:03:29 -0300

    Adicionando validação no cadastro de produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,13 +1,22 @@
 class ProdutosController < ApplicationController
-
+    
     def index
         @listaProduto = Produto.order(:nome).limit 10
     end
 
     def create
         valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade_estoque
-        produto = Produto.create valores
-        redirect_to new_produto_url
+        @produto = Produto.new valores
+        if @produto.save
+            flash[:notice] = "Produto cadastrado com sucesso"
+            redirect_to root_url
+        else
+            render :new
+        end
+    end
+
+    def new
+        @produto = Produto.new
     end
 
     def destroy
@@ -20,5 +29,5 @@ class ProdutosController < ApplicationController
         @nome_busca = params[:nome]
         @produtos = Produto.where "nome like ?", "%#{@nome_busca}%"
     end
-    
 end
+
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,6 @@
 class Produto < ApplicationRecord
+
+    validates :quantidade_estoque, presence: true
+    validates :nome, length: {minimum: 5}
+    
 end
