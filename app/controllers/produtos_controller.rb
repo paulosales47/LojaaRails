9adbf6cb656e0dd0259450fb23e03bf269160e1f
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-03-11 18:19:42 -0300

    Finalizando curso - Adicionada edição dos produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,28 +1,30 @@
 class ProdutosController < ApplicationController
     
+    before_action :set_produto, only: [:edit, :update, :destroy]
+
     def index
         @listaProduto = Produto.order(:nome).limit 10
     end
 
     def create
-        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade_estoque, :departamento_id
+        valores = produto_params
         @produto = Produto.new valores
         if @produto.save
             flash[:notice] = "Produto cadastrado com sucesso"
             redirect_to root_url
         else
+            @departamentos = Departamento.all
             render :new
         end
     end
 
     def new
         @produto = Produto.new
-        @departamentos = Departamento.all
+        renderiza(:new)
     end
 
     def destroy
-        id = params[:id]
-        Produto.destroy id
+        Produto.destroy 
         redirect_to root_url
     end
 
@@ -30,5 +32,36 @@ class ProdutosController < ApplicationController
         @nome_busca = params[:nome]
         @produtos = Produto.where "nome like ?", "%#{@nome_busca}%"
     end
+
+    def edit
+        renderiza(:edit)
+    end
+
+    def update
+        valores = produto_params
+        if @produto.update valores
+            flash[:notice] = "Produto autualizado com sucesso"
+            redirect_to root_url
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def renderiza(view)
+        @departamentos = Departamento.all
+        render view
+    end
+
+    def set_produto
+        id = params[:id]
+        @produto = Produto.find(id)
+    end
+
+    def produto_params
+        params.require(:produto).permit :nome, :preco, :descricao, :quantidade_estoque, :departamento_id
+    end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :departamentos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #Pagina PRODUTO
-  resources :produtos, only: [:new, :create, :destroy]
+  resources :produtos, only: [:new, :create, :destroy, :edit, :update]
   get "/produtos/busca" => "produtos#busca", as: :busca_produto
   #RAIZ
   root "produtos#index"
