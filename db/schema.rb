4d3df01c9869416aa5730a650ab6966c6260aaa0
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-03-11 16:16:24 -0300

    Utilizando scaffold para gerar arquivos de departamento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304194316) do
+ActiveRecord::Schema.define(version: 20180311175753) do
+
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
